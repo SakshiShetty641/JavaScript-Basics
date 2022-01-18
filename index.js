35c1f0d868f91532defc269c442cd3b8ffc1a566
Sakshi Shetty <sakshishetty@Sakshis-MacBook-Pro.local>
2022-01-18 15:06:31 +0530

    Add. To understand JavaScript variables and JavaScript Datatypes

---
+++ index.js
@@ -0,0 +1,36 @@
+//1- Ways to print in javascript
+console.log("Hello World")
+alert("Sakshi Shetty")
+document.write("This is a document")
+
+//2- Javascript Console Log
+console.log("Welcome to the program")
+console.warn("This is a warning")
+console.error("This is an error")
+
+//3- Javascript Variables
+var no1 = 10;
+var no2 = 20;
+console.log(no1 + no2);
+
+//4- Primitive Datatypes in Javascript
+//Numbers
+var no1 = 10;
+//String
+var str1 = "Hello World";
+
+console.log(marks)
+//Booleans
+var a = true;
+var b = false;
+
+//5- Reference Datatypes in Javascript
+//Object
+var marks = {
+    ravi: 34,
+    sashi: 56
+}
+
+//Array
+var arr = [1, 2, 3 ,4]
+console.log(arr)
